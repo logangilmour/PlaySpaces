18eb42e6538d580472d64d0f2b7b5c00e96dd203
Mitch Lindgren <lindgren.mitch@gmail.com>
2012-04-01 19:00:25 -0600

    Very basic map and content switching

---
+++ web/public/MapElement.js
@@ -0,0 +1,65 @@
+/* Map elements have the following properties:
+
+Visibility state
+Location
+Image
+Listens to events to change visibility 
+
+Requires jQuery
+*/
+
+function MapElement(content)
+{
+  // this.visible = false;
+  this.element = null;
+  this.locationX = 0;
+  this.locationY = 0;
+  this.content = content;
+  this.image = 'images/dot_off.png';
+  this.handleEvent = function() { };
+}
+
+MapElement.prototype.add = function(map, x, y)
+{
+  this.locationX = x;
+  this.locationY = y;
+  
+  this.element = $('<img src="images/dot_off.png" class="map_element">')
+                 .toggle()
+                 .css('bottom', this.locationY + 'px')
+                 .css('left', this.locationX + 'px');
+
+  /* Set up click handler to switch the content box to the content.
+     TODO:
+       - Content box ID shouldn't be hard coded
+       - Need to notify other map elements using event bus */
+  this.element.click(function()
+  {
+    $("#content_box").load(this.content);
+  });
+
+  // Add the element to the map
+  $(map).append(this.element);
+}
+
+MapElement.prototype.toggleVisibility = function()
+{
+  this.element.toggle();
+}
+
+/*
+EventBus.prototype.add = function(callback)
+{
+    if(typeof callback != "function")
+    {
+        callback = new Function(callback);
+    }
+    this.listeners[this.listeners.length]=callback;
+}
+EventBus.prototype.fire = function(name,value)
+{
+    for(var i=0; i<this.listeners.length; i++){
+        this.listeners[i](name,value);
+    }
+}
+*/
