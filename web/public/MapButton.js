e36c7741eb8d47f9ab43b1d53b9f8b9d1689f099
Robin Miller <remiller@ualberta.ca>
2012-04-10 15:50:33 -0600

    Got dots listening to events again. Not done press'n'holds, though.

--- web/public/MapButton.js
+++ web/public/MapButton.js
@@ -1,16 +1,34 @@
 /**
- * Pressables paracitically extend MapElements.
+ * Map Buttons paracitically extend MapElements.
  * They have content that they can send to the Content box when pressed.
  * Pressables can have content that appears/plays only when held added to them.
 */
-function MapPressable(image, content)
+function MapButton(image, done, content)
 {
-  var that = new MapElement(image);
+  var that = new MapElement(image, done);
   that.content = content;
   that.setHoldContent = function(content)
   {
-	this.holdContent = content;
+		this.holdContent = content;
   }
+	
+	var contentBox = $("#content_box");
+	this.content="content/one.html";
+	var contentError = function (response, status, xhr)
+	{
+		if (status == "error")
+		{
+			var msg = "Sorry but there was an error: ";
+			contentBox.html(msg + xhr.status + " " + xhr.statusText);
+		}
+	}
+	
+	that.element.click(function()
+	{
+		if(content){
+			contentBox.load(content, contentError);
+		}
+	});
 
   return that;
 }
