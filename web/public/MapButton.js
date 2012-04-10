09b4762c4b8aba9504878600fa33fc57e296a651
Robin Miller <remiller@ua02.cs.ualberta.ca>
2012-04-10 14:49:26 -0600

    Fixed merge bug where it was losing the map image

--- web/public/MapButton.js
+++ web/public/MapButton.js
@@ -1,11 +1,16 @@
 /**
  * Pressables paracitically extend MapElements.
  * They have content that they can send to the Content box when pressed.
+ * Pressables can have content that appears/plays only when held added to them.
 */
 function MapPressable(image, content)
 {
   var that = new MapElement(image);
   that.content = content;
+  that.setHoldContent = function(content)
+  {
+	this.holdContent = content;
+  }
 
   return that;
 }
--- web/public/MapElement.js
+++ web/public/MapElement.js
@@ -19,22 +19,6 @@ function MapElement(element,done)
     element.prepend(this.image);
     element.css({margin:"0px",padding:"0px"});
 }
-/**
- * Pressables paracitically extend MapElements.
- * They have content that they can send to the Content box when pressed.
- * Pressables can have content that appears/plays only when held added to them.
-*/
-function MapPressable(image, content)
-{
-  var that = new MapElement(image);
-  that.content = content;
-  that.setHoldContent = function(content)
-  {
-	this.holdContent = content;
-  }
-
-  return that;
-}
 
 MapElement.prototype.add = function(map, x, y)
 {
