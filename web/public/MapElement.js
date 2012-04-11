2ae7dec49865cf72d429d4e8d9fa40907a007282
Robin Miller <remiller@ualberta.ca>
2012-04-10 20:21:28 -0600

    Got holdemups to work

--- web/public/MapElement.js
+++ web/public/MapElement.js
@@ -18,8 +18,8 @@ function MapElement(element,done)
     element.css({position:"absolute"});
     element.prepend(this.image);
     element.css({margin:"0px",padding:"0px"});
-    
 }
+
 /*
 MapElement.prototype.add = function(map, x, y)
 {
@@ -76,6 +76,7 @@ MapElement.prototype.add = function(map, x, y)
   $(map).append(this.element);
 }
 */
+
 // set position of element - to be called by the map in order to place
 // correctly relative to size.
 MapElement.prototype.setPos = function(x, y,width)
